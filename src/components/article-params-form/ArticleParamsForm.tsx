06f3dc760c297410b5522cc6ca9f707269331f57
Nikita Kolankov <kolankovnikita@gmail.com>
2025-06-18 18:36:53 +0300

    Реализовано поведение формы кастомизации

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,121 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
+import { FormEvent, useState } from 'react';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+
+export type ArticleParamsFormProps = {
+	setAppState: (value: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const { setAppState } = props;
+	const [isOpened, setIsOpened] = useState(false);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleToggle = () => {
+		setIsOpened((currentIsOpened) => !currentIsOpened);
+	};
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setAppState(formState);
+		setIsOpened(false);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		setAppState(defaultArticleState);
+		setIsOpened(false);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpened} onClick={handleToggle} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpened,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text as='h1' size={31} weight={800} uppercase dynamicLite>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(selected) =>
+							setFormState((currentFormState) => ({
+								...currentFormState,
+								fontFamilyOption: selected,
+							}))
+						}
+					/>
+					<RadioGroup
+						title='Размер шрифта'
+						name='fontSizeOption'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(selected) =>
+							setFormState((currentFormState) => ({
+								...currentFormState,
+								fontSizeOption: selected,
+							}))
+						}
+					/>
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(selected) =>
+							setFormState((currentFormState) => ({
+								...currentFormState,
+								fontColor: selected,
+							}))
+						}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(selected) =>
+							setFormState((currentFormState) => ({
+								...currentFormState,
+								backgroundColor: selected,
+							}))
+						}
+					/>
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(selected) =>
+							setFormState((currentFormState) => ({
+								...currentFormState,
+								contentWidth: selected,
+							}))
+						}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
